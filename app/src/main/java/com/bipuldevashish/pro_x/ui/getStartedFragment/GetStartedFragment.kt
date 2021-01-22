5ad21010d04cbc24260c95f53128bcee822ed9aa
Bipul Devashish <bipuldevashish@gmail.com>
2021-01-22 20:53:36 +0530

    fragment navigation.

--- app/src/main/java/com/bipuldevashish/pro_x/ui/getStartedFragment/GetStartedFragment.kt
+++ app/src/main/java/com/bipuldevashish/pro_x/ui/getStartedFragment/GetStartedFragment.kt
@@ -3,10 +3,12 @@ package com.bipuldevashish.pro_x.ui.getStartedFragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bipuldevashish.pro_x.R
 import com.bipuldevashish.pro_x.databinding.FragmentGetStartedBinding
 
 
-class GetStartedFragment : Fragment() {
+class GetStartedFragment : Fragment(R.layout.fragment_get_started) {
 
     private var _binding: FragmentGetStartedBinding? = null
     private val binding get() = _binding!!
@@ -16,6 +18,14 @@ class GetStartedFragment : Fragment() {
 
         _binding = FragmentGetStartedBinding.bind(view)
 
+        binding.loginBtn.setOnClickListener {
+            val action = GetStartedFragmentDirections.actionGetStartedFragmentToLoginFragment2()
+            findNavController().navigate(action)
+        }
 
+        binding.signupBtn.setOnClickListener {
+            val action = GetStartedFragmentDirections.actionGetStartedFragmentToSignup2()
+            findNavController().navigate(action)
+        }
     }
 }
